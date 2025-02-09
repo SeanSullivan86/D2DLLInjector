b67f230bc73da297644b60946786181db174f9a9
SeanSullivan <sully71486@gmail.com>
2025-02-09 11:30:27 -0800

    Working on being able to pass parameters to the injected DLL

--- D2DLLInjector/D2DLLInjector.cpp
+++ D2DLLInjector/D2DLLInjector.cpp
@@ -5,155 +5,10 @@
 #include <chrono>
 #include <psapi.h>
 
-#ifndef ArraySize
-#define ArraySize(x) (sizeof((x)) / sizeof((x)[0]))
-#endif
+// forward declarations
+void exitAndReportWin32APIFailureIfConditionTrue(bool shouldExit, const char* errorDescription);
+uint32_t GetModuleAddressWithinProcess(DWORD processId, const char* targetModuleName);
 
-std::string GetLastErrorStdStr()
-{
-    DWORD error = GetLastError();
-    if (error)
-    {
-        LPVOID lpMsgBuf;
-        DWORD bufLen = FormatMessage(
-            FORMAT_MESSAGE_ALLOCATE_BUFFER |
-            FORMAT_MESSAGE_FROM_SYSTEM |
-            FORMAT_MESSAGE_IGNORE_INSERTS,
-            NULL,
-            error,
-            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-            (LPTSTR)&lpMsgBuf,
-            0, NULL);
-        if (bufLen)
-        {
-            LPCSTR lpMsgStr = (LPCSTR)lpMsgBuf;
-            std::string result(lpMsgStr, lpMsgStr + bufLen);
-
-            LocalFree(lpMsgBuf);
-
-            return result;
-        }
-    }
-    return std::string();
-}
-
-void GetModuleInfo(DWORD processId) {
-    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
-    if (hProcess == NULL) {
-        printf("OpenProcess failed\n");
-        std::cout << GetLastErrorStdStr() << std::endl;
-        return;
-    }
-
-    HMODULE modules[1000];
-    DWORD outputBytes;
-    char moduleName[1000];
-
-    BOOL x = EnumProcessModulesEx(hProcess, modules, sizeof(modules), &outputBytes, LIST_MODULES_32BIT);
-    if (x == 0) {
-        printf("EnumProcessModulesEx failed\n");
-        std::cout << GetLastErrorStdStr() << std::endl;
-        CloseHandle(hProcess);
-        return;
-    }
-
-    int i = 0;
-    for (i = 0; i < (outputBytes / sizeof(HMODULE)); i++) {
-        int r = GetModuleBaseName(hProcess, modules[i], moduleName, sizeof(moduleName));
-        if (r == 0) {
-            printf("GetModuleBaseName failed\n");
-            std::cout << GetLastErrorStdStr() << std::endl;
-            CloseHandle(hProcess);
-            return;
-        }
-        printf("Module %s\n", moduleName);
-    }
-
-    CloseHandle(hProcess);
-}
-
-uint32_t GetModuleAddressWithinProcess(DWORD processId, const char* targetModuleName) {
-    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
-    if (hProcess == NULL) {
-        printf("OpenProcess failed\n");
-        std::cout << GetLastErrorStdStr() << std::endl;
-        return -1;
-    }
-
-    HMODULE modules[1000];
-    DWORD outputBytes;
-    char moduleName[1000];
-
-    BOOL x = EnumProcessModulesEx(hProcess, modules, sizeof(modules), &outputBytes, LIST_MODULES_32BIT);
-    if (x == 0) {
-        printf("EnumProcessModulesEx failed\n");
-        std::cout << GetLastErrorStdStr() << std::endl;
-        CloseHandle(hProcess);
-        return -1;
-    }
-
-    int i = 0;
-    for (i = 0; i < (outputBytes / sizeof(HMODULE)); i++) {
-        int r = GetModuleBaseName(hProcess, modules[i], moduleName, sizeof(moduleName));
-        if (r == 0) {
-            printf("GetModuleBaseName failed\n");
-            std::cout << GetLastErrorStdStr() << std::endl;
-            CloseHandle(hProcess);
-            return -1;
-        }
-        if (stricmp(moduleName, targetModuleName) == 0) {
-            HMODULE foundModule = modules[i];
-            MODULEINFO moduleInfo;
-            BOOL moduleInfoCallResult = GetModuleInformation(hProcess, foundModule, &moduleInfo, sizeof(MODULEINFO));
-            if (moduleInfoCallResult == 0) {
-                printf("GetModuleInformation failed\n");
-                std::cout << GetLastErrorStdStr() << std::endl;
-                CloseHandle(hProcess);
-                return -1;
-            }
-
-            return (uint32_t) moduleInfo.lpBaseOfDll;
-        }
-        printf("Module %s\n", moduleName);
-    }
-
-    CloseHandle(hProcess);
-    return -1;
-}
-
-/*
-uint64_t GetModuleAddressWithinProcess(DWORD th32ProcessID, const char* moduleName) {
-    MODULEENTRY32 entry;
-    entry.dwSize = sizeof(PROCESSENTRY32);
-
-    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, th32ProcessID);
-    if (snapshot == INVALID_HANDLE_VALUE) {
-        printf("CreateToolhelp32Snapshot failed\n");
-        std::cout << GetLastErrorStdStr() << std::endl;
-        return -1;
-    }
-
-    if (Module32First(snapshot, &entry)) {
-        do {
-            printf("ModuleName : %s\n", entry.szModule);
-            if (stricmp(entry.szModule, moduleName) == 0) {
-                CloseHandle(snapshot);
-                return (uint64_t) entry.modBaseAddr;
-            }
-        } while (Module32Next(snapshot, &entry) == TRUE);
-    }
-
-    printf("No module found with name %s\n", moduleName);
-
-    CloseHandle(snapshot);
-    return -1;
-}
-*/
-
-
-
-
-FILE* outputFile;
 
 int main()
 {
@@ -163,14 +18,14 @@ int main()
 
     HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
     int i = 0;
-    int processId = 0;
+    int d2ProcessId = 0;
     if (Process32First(snapshot, &entry) == TRUE)
     {
         do {
             i++;
             if (stricmp(entry.szExeFile, "Game.exe") == 0)
             {
-                processId = entry.th32ProcessID;
+                d2ProcessId = entry.th32ProcessID;
                 break;
             }
         } while (Process32Next(snapshot, &entry) == TRUE);
@@ -178,8 +33,8 @@ int main()
 
     CloseHandle(snapshot);
 
-    if (processId == 0) { return 0; }
-    std::cout << "Found d2 process id : " << processId << std::endl;
+    if (d2ProcessId == 0) { return 0; }
+    std::cout << "Found d2 process id : " << d2ProcessId << std::endl;
 
 
     /* START OF "Acquire debug privilege" code" */
@@ -204,67 +59,117 @@ int main()
     std::cout << "Acquired debug privilege\n";
     /* END OF "Acquire debug privilege" code" */
 
-    uintptr_t localDllBase = (uintptr_t) LoadLibraryA(pathToDllToInject);
-    if (localDllBase == NULL) {
-        printf("Injecting DLL to our process failed\n");
-        std::cout << GetLastErrorStdStr() << std::endl;
-        return 1;
-    }
-    else {
-        printf("Local address of injected dll %i\n", localDllBase);
+
+    HANDLE d2Process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, d2ProcessId);
+
+    // kernel32.dll is always loaded at the same virtual memory location in every win32 process, so we don't need to worry about
+    // the address being different in the D2 process than our process
+    uint32_t addressOfLoadLibraryFunc = (uint32_t) GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+    exitAndReportWin32APIFailureIfConditionTrue(addressOfLoadLibraryFunc == NULL, "Failed to find address of LoadLibraryA");
+
+
+
+    LPVOID remoteAddrForDllPath = (LPVOID)VirtualAllocEx(d2Process, NULL, strlen(pathToDllToInject), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+    exitAndReportWin32APIFailureIfConditionTrue(remoteAddrForDllPath == NULL, "Could not allocate memory in D2 process");
+
+
+    BOOL result = WriteProcessMemory(d2Process, remoteAddrForDllPath, pathToDllToInject, strlen(pathToDllToInject), NULL);
+    exitAndReportWin32APIFailureIfConditionTrue(result == FALSE, "Could not write to D2 process memory");
+
+
+    HANDLE threadId = CreateRemoteThread(d2Process, NULL, 0, (LPTHREAD_START_ROUTINE) addressOfLoadLibraryFunc, remoteAddrForDllPath, NULL, NULL);
+    exitAndReportWin32APIFailureIfConditionTrue(threadId == NULL, "Remote Thread could not be started");
+
+    // wait for the remote thread to do its work injecting D2LootDropLooper.dll to the D2 process
+    uint32_t injectedDllAddressInD2Process = -1;
+    int j = 0;
+    while (injectedDllAddressInD2Process == -1) {
+        if (j > 10) {
+            printf("Could not find D2LootDropLooper.dll in D2 process after 10 seconds\n");
+            ExitProcess(1);
+        }
+        Sleep(1000);
+        injectedDllAddressInD2Process = GetModuleAddressWithinProcess(d2ProcessId, "D2LootDropLooper.dll");
+        printf("injectedDllAddressInD2Process %i\n", injectedDllAddressInD2Process);
+        j++;
     }
 
-    MODULEINFO moduleInfo;
-    BOOL moduleInfoCallResult = GetModuleInformation(GetCurrentProcess(), (HMODULE)localDllBase, &moduleInfo, sizeof(MODULEINFO));
-    if (moduleInfoCallResult == 0) {
-        printf("GetModuleInformation failed\n");
-        return 1;
-    }
-    printf("Address: %i\n", (uint32_t)moduleInfo.lpBaseOfDll);
-
-    // GetModuleInfo(20400);
+    HMODULE dllInjectedInOurProcess = LoadLibraryA(pathToDllToInject);
+    exitAndReportWin32APIFailureIfConditionTrue(dllInjectedInOurProcess == NULL, "LoadLibraryA failed");
+    printf("Local address of injected dll %i\n", dllInjectedInOurProcess);
 
     uint32_t injectedDllAddressInOurProcess = GetModuleAddressWithinProcess(GetCurrentProcessId(), "D2LootDropLooper.dll");
     printf("injectedDllAddressInOurProcess %i\n", injectedDllAddressInOurProcess);
 
-    uint32_t gameexeaddress = GetModuleAddressWithinProcess(20400 /* GetCurrentProcessId() */, "Game.exe");
-    printf("game.exe address %i\n", gameexeaddress);
+    uint32_t functionAddressInOurProcess = (uint32_t)GetProcAddress(dllInjectedInOurProcess, "InitLootDropLooper");
+    uint32_t functionOffsetWithinDll = functionAddressInOurProcess - injectedDllAddressInOurProcess;
+
+    uint32_t functionAddressInD2Process = injectedDllAddressInD2Process + functionOffsetWithinDll;
 
 
-    /*
-
-    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
-
-    LPVOID addr = (LPVOID)GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
-    if (addr == NULL) {
-        printf("Error: the LoadLibraryA function was not found inside kernel32.dll library.\n");
-    }
-
-    LPVOID arg = (LPVOID)VirtualAllocEx(hProcess, NULL, strlen(buffer), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-    if (arg == NULL) {
-        printf("Error: the memory could not be allocated inside the chosen process.\n");
-        std::cout << GetLastErrorStdStr() << std::endl;
-    }
-
-    int n = WriteProcessMemory(hProcess, arg, buffer, strlen(buffer), NULL);
-    if (n == 0) {
-        printf("Error: there was no bytes written to the process's address space.\n");
-    }
-
-    HANDLE threadID = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)addr, arg, NULL, NULL);
-    if (threadID == NULL) {
-        printf("Error: the remote thread could not be created.\n");
-    }
-    else {
-        printf("Success: the remote thread was successfully created.\n");
-    }
+    LPVOID remoteAddrToPassParams = (LPVOID)VirtualAllocEx(d2Process, NULL, strlen(pathToDllToInject), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+    exitAndReportWin32APIFailureIfConditionTrue(remoteAddrToPassParams == NULL, "Could not allocate memory in D2 process");
 
 
-    CloseHandle(hProcess);
-    */
+    uint32_t paramToPassToInjectedDll = 1;
+    result = WriteProcessMemory(d2Process, remoteAddrToPassParams, &paramToPassToInjectedDll, sizeof(uint32_t), NULL);
+    exitAndReportWin32APIFailureIfConditionTrue(result == FALSE, "Could not write to D2 process memory");
+
+    threadId = CreateRemoteThread(d2Process, NULL, 0, (LPTHREAD_START_ROUTINE) functionAddressInD2Process, remoteAddrToPassParams, NULL, NULL);
+    exitAndReportWin32APIFailureIfConditionTrue(threadId == NULL, "Remote Thread could not be started");
+
+
+    CloseHandle(d2Process);
+    
 
     return 0;
 }
 
 
 
+
+void exitAndReportWin32APIFailureIfConditionTrue(bool shouldExit, const char* errorDescription) {
+    if (!shouldExit) return;
+
+    DWORD error = GetLastError();
+    if (error) {
+        LPVOID lpMsgBuf;
+        DWORD bufLen = FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+            NULL, error, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&lpMsgBuf, 0, NULL);
+        if (bufLen) {
+            printf("Error from Win32 API : %s : %s \n", errorDescription, (char*)lpMsgBuf);
+            LocalFree(lpMsgBuf);
+        }
+    }
+    ExitProcess(1);
+}
+
+uint32_t GetModuleAddressWithinProcess(DWORD processId, const char* targetModuleName) {
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
+    exitAndReportWin32APIFailureIfConditionTrue(hProcess == NULL, "OpenProcess failed");
+
+    HMODULE modules[1000];
+    DWORD outputBytes;
+    char moduleName[1000];
+
+    BOOL result = EnumProcessModulesEx(hProcess, modules, sizeof(modules), &outputBytes, LIST_MODULES_32BIT);
+    exitAndReportWin32APIFailureIfConditionTrue(result == FALSE, "EnumProcessModulesEx failed");
+
+    int i = 0;
+    for (i = 0; i < (outputBytes / sizeof(HMODULE)); i++) {
+        DWORD len = GetModuleBaseName(hProcess, modules[i], moduleName, sizeof(moduleName));
+        exitAndReportWin32APIFailureIfConditionTrue(len == 0, "GetModuleBaseName failed");
+
+        if (stricmp(moduleName, targetModuleName) == 0) {
+            HMODULE foundModule = modules[i];
+            MODULEINFO moduleInfo;
+            BOOL moduleInfoCallResult = GetModuleInformation(hProcess, foundModule, &moduleInfo, sizeof(MODULEINFO));
+            exitAndReportWin32APIFailureIfConditionTrue(moduleInfoCallResult == FALSE, "GetModuleInformation failed");
+            CloseHandle(hProcess);
+            return (uint32_t)moduleInfo.lpBaseOfDll;
+        }
+    }
+
+    CloseHandle(hProcess);
+    return -1;
+}
